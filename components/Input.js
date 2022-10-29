40c15db96e5b21408e745b6aa56d24e8382757d3
Isaiah Desta <isaiahdesta@Isaiahs-MBP.attlocal.net>
2022-10-29 14:24:37 -0400

    input for tweeting

--- components/Input.js
+++ components/Input.js
@@ -1,7 +1,32 @@
-import React from "react";
+import { useSession } from "next-auth/react";
+import React, { useState } from "react";
 
 const Input = () => {
-  return <div>Input</div>;
+  const [loading, isLoading] = useState(false);
+  const [input, setInput] = useState("");
+  const { data: session } = useSession();
+  return (
+    <div className={`mt-4 px-4 ${loading && "opacity-60"}`}>
+      <div className="grid grid-cols-[48px,1fr] gap-4">
+        <div>
+          <img
+            src={session?.user?.image}
+            alt=""
+            className="h-12 w-12 rounded-full object-contain"
+          />
+        </div>
+        <div className="w-[90%]">
+          <textarea
+            className="w-[100%] bg-transparent outline-none text-[20px]"
+            rows="2"
+            placeholder="What's Happening?"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Input;
